691bce706bade3945ca866b2a1a7756d1d010070
Satyam jha <satyam1170.be21@chitkarauniversity.edu.in>
2024-09-29 13:36:05 +0530

    Time: 50 ms (32.72%) | Memory: 12.8 MB (96.78%) - LeetSync

--- interview150/1-two-sum/two-sum.cpp
+++ interview150/1-two-sum/two-sum.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        map<int, int> mp;
+        //map<int, int> mp;
 
         for(int i=0;i<n;i++){
-            int remaining=target-nums[i];
-            if(mp.find(remaining)!=mp.end()){
-                return {mp[remaining],i};
+            //int remaining=target-nums[i];
+            // if(mp.find(remaining)!=mp.end()){
+            //     return {mp[remaining],i};
+            // }
+            // mp[nums[i]]=i;
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
             }
-            mp[nums[i]]=i;
         }
         
       return {};  
